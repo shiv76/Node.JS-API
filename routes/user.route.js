891ada7c6b9f00fa5245e5688c28d33b4b693a37
Shiv <shivamshiv76@gmail.com>
2021-01-30 19:29:17 +0530

    Initial commit

---
+++ routes/user.route.js
@@ -0,0 +1,69 @@
+// user.route.js
+
+const express = require('express');
+const userRoutes = express.Router();
+
+// Require User model in our routes module
+let User = require('../models/User');
+
+// Defined store route
+userRoutes.route('/add').post(function (req, res) {
+  let user = new User(req.body);
+  user.save()
+    .then(user => {
+      res.status(200).json({'User': 'user has been added successfully'});
+    })
+    .catch(err => {
+    res.status(400).send("unable to save to database");
+    });
+});
+
+// Defined get data(index or listing) route
+userRoutes.route('/').get(function (req, res) {
+  User.find(function (err, users){
+    if(err){
+      console.log(err);
+    }
+    else {
+      res.json(users);
+    }
+  });
+});
+
+// Defined edit route
+userRoutes.route('/edit/:id').get(function (req, res) {
+  let id = req.params.id;
+  User.findById(id, function (err, user){
+      res.json(user);
+  });
+});
+
+//  Defined update route
+userRoutes.route('/update/:id').post(function (req, res) {
+  User.findById(req.params.id, function(err, user) {
+    if (!user)
+      res.status(404).send("Record not found");
+    else {
+      user.firstName = req.body.firstName;
+      user.lastName = req.body.lastName;
+      user.email = req.body.email;
+      user.jobTitle = req.body.jobTitle;
+      user.save().then(user=> {
+          res.json('Update complete');
+      })
+      .catch(err => {
+            res.status(400).send("unable to update the database");
+      });
+    }
+  });
+});
+
+// Defined delete | remove | destroy route
+userRoutes.route('/delete/:id').get(function (req, res) {
+    User.findByIdAndRemove({_id: req.params.id}, function(err, user){
+        if(err) res.json(err);
+        else res.json('Successfully removed');
+    });
+});
+
+module.exports = userRoutes;
